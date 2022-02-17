b672a83cd8f6ddf8c2d0efdd7543f10980b0de38
Slipscape <99834400+slipscape@users.noreply.github.com>
2022-02-16 21:12:45 -0500

    2.16.2022 9:12 PM
    Implemented working gravitational formulas

--- classes_functions.py
+++ classes_functions.py
@@ -1,23 +1,48 @@
+import pygame
 import numpy as np
 
-G = 6.675 * 10**-11
+G = 100#6.675 * 10**-11
 
 class gravobject:
+    counter = 0
+
     def __init__(self, mass, rval, starting_coordinates, starting_vel, color, r_or_d = "radius"):
         self.mass = mass
         if r_or_d == "radius":
             self.radius = rval
         elif r_or_d == "density":
             self.radius = np.cbrt((3*mass)/(4*np.pi*rval))
-        self.velocity = starting_vel
-        self.coordinates = np.array([starting_coordinates[0], starting_coordinates[1]])
+        self.velocity = np.array([float(starting_vel[0]), float(starting_vel[1])])
+        self.coordinates = np.array([float(starting_coordinates[0]), float(starting_coordinates[1])])
         self.color = color
-        self.acceleration = np.array([0, 0])
+        self.acceleration = np.array([0.0, 0.0])
+        self.id = gravobject.counter
+        gravobject.counter += 1
+    
+    def render(self, window, scale):
+        pygame.draw.circle(window, self.color, (self.coordinates[0] * scale, self.coordinates[1] * scale), self.radius * scale)
 
 # F = ma
 # a = F/m
-def gravinteract(object1, object2, delta):
-    gravitational_force = (object1.mass * object2.mass) / (np.sqrt((object1.cooordinates[0] - object2.cooordinates[0])**2 + (object1.coordinates[1] - object2.coordinates[1])**2)) * G
-    accel1 = gravitational_force / object1.mass
-    accel2 = gravitational_force / object2.mass
-    accel1 = np.array([])
+def gravinteract(object_list):
+    archive = []
+    for object1 in object_list:
+        for object2 in object_list:
+            if (object1.id, object2.id) not in archive and (object2.id, object1.id) not in archive and not object1 == object2:
+                dif_x = object1.coordinates[0] - object2.coordinates[0]
+                dif_y = object1.coordinates[1] - object2.coordinates[1]
+                dif_hyp = np.sqrt(dif_x**2 + dif_y**2)
+                dif_angle = np.arctan2(dif_x, dif_y)
+                gravitational_force = (object1.mass * object2.mass) / dif_hyp**2 * G
+                accel1 = gravitational_force / object1.mass
+                accel2 = gravitational_force / object2.mass
+                object1.acceleration += np.array([np.sin(dif_angle), np.cos(dif_angle)]) * -accel1
+                object2.acceleration += np.array([np.sin(dif_angle), np.cos(dif_angle)]) * accel2
+                #np.sqrt(object1.acceleration[1]**2 + object1.acceleration[1]**2))
+                archive.append((object1.id, object2.id))
+
+def apply_acceleration(object_list, delta):
+    for object in object_list:
+        object.velocity += object.acceleration
+        object.coordinates += object.velocity * (delta / 16.7)
+        object.acceleration = np.array([0.0, 0.0])
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import numpy as np
 
-import classes_functions as Gravsimcf
+import classes_functions as Gravsim_cf
 
 Width = 900
 Height = 500
@@ -8,11 +9,24 @@ Window = pygame.display.set_mode((Width, Height))
 pygame.display.set_caption("Gravsim 0.0.1")
 
 Colors = {
-    "light_grey" : (200, 200, 200)
+    "light_grey" : (200, 200, 200),
+    "orange" : (255, 100, 0),
+    "blue" : (25, 25, 255),
+    "green" : (25, 255, 25)
 }
 
+Scale = 2
+
 objects = []
-objects.append(Gravsimcf.gravobject(1, 1, [0, 0], [-1, 0], Colors['light_grey']))
+objects += [Gravsim_cf.gravobject(0.005, 1, [220.0, 25.0], np.array([1, 0.0]), Colors['green']), Gravsim_cf.gravobject(0.001, 2, [200.0, 50.0], np.array([1, 0.0]), Colors['light_grey']), Gravsim_cf.gravobject(1, 6, [200.0, 125.0], np.array([0.0, 0.0]), Colors['blue'])]
+
+def gravloop(delta):
+    Gravsim_cf.gravinteract(objects)
+
+    Gravsim_cf.apply_acceleration(objects, delta)
+
+    for object in objects:
+        object.render(Window, Scale)
 
 def main():
     clock = pygame.time.Clock()
@@ -21,10 +35,15 @@ def main():
     while run:
         delta = clock.tick(60)
 
+        Window.fill((0, 0, 0))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-    
+
+        gravloop(delta)
+        pygame.display.update()
+        
     pygame.quit()
 
 
